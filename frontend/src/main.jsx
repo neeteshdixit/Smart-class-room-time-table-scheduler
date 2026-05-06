b7cf9f2da023d43edfcdb55b075b691773887ac2
neeteshdixit <dixitneetesh857@gmail.com>
2026-05-07 01:09:47 +0530

    Configured app providers and initialization

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,24 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import { CursorProvider } from "./context/CursorContext";
+import CustomCursor from "./components/CustomCursor";
+import AuroraBackground from "./components/AuroraBackground";
+import SmoothScroll from "./components/SmoothScroll";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <CursorProvider>
+          <SmoothScroll>
+            <CustomCursor />
+            <AuroraBackground>
+              <App />
+            </AuroraBackground>
+          </SmoothScroll>
+        </CursorProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
