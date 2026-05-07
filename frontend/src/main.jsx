ed0cf64df784280f18f4bdc0aff1b4d184b1f2ee
neeteshdixit <dixitneetesh857@gmail.com>
2026-05-07 11:19:10 +0530

    Improved frontend initialization and providers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { CursorProvider } from "./context/CursorContext";
 import CustomCursor from "./components/CustomCursor";
 import AuroraBackground from "./components/AuroraBackground";
 import SmoothScroll from "./components/SmoothScroll";
+import ErrorBoundary from "./components/ErrorBoundary";
 import "./styles.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <CursorProvider>
-          <SmoothScroll>
-            <CustomCursor />
-            <AuroraBackground>
-              <App />
-            </AuroraBackground>
-          </SmoothScroll>
-        </CursorProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AuthProvider>
+            <CursorProvider>
+              <SmoothScroll>
+                <CustomCursor />
+                <AuroraBackground>
+                  <App />
+                </AuroraBackground>
+              </SmoothScroll>
+            </CursorProvider>
+          </AuthProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
